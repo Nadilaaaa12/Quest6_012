d1c6de3d5898a580b4d694db475bdd07f6c72027
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-11-22 15:48:54 +0700

    untuk mengelola navigasi antar halaman dan menghubungkan data dari ViewModel ke UI

--- app/src/main/java/com/example/p8navigasidata/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/p8navigasidata/navigation/PengelolaHalaman.kt
@@ -1,8 +1,31 @@
 package com.example.p8navigasidata.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.p8navigasidata.ui.view.screen.MahasiswaFormView
+import com.example.p8navigasidata.ui.view.screen.RencanaStudyView
+import com.example.p8navigasidata.ui.view.screen.SplashView
+import com.example.p8navigasidata.ui.view.viewmodel.MahasiswaViewModel
+import com.example.p8navigasidata.ui.view.viewmodel.RencanaStudyViewModel
+
 enum class Halaman {
     Splash,
     Mahasiswa,
     Matakuliah,
     Tampil
 }
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
