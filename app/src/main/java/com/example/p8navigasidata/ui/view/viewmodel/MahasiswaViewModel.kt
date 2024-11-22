61bb84da5fc9f0315e1b03df5f859a7e804165d1
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2024-11-22 15:17:44 +0700

    menyediakan mekanisme yang bersih dan reaktif untuk mengelola data mahasiswa di aplikasi

--- app/src/main/java/com/example/p8navigasidata/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/p8navigasidata/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.p8navigasidata.ui.view.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.p8navigasidata.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MahasiswaViewModel : ViewModel(){
+
+    private val mahasiswaStateUI = MutableStateFlow(Mahasiswa())
+    val mahasiswaUIState: StateFlow<Mahasiswa> = mahasiswaStateUI.asStateFlow()
+    fun saveDataMahasiswa(ls: MutableList<String>){
+        mahasiswaStateUI.value = Mahasiswa(
+            nim = ls[0],
+            nama = ls[1],
+            email = ls[2]
+        )
+    }
 }
